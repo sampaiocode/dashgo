34b80ce15ca8872a4c5c573fe34bb95be9c68662
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-09-29 22:14:18 -0300

    feat: adicionando o componente header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,35 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
 import { Input } from '../components/Form/Input';
 
+import Head from 'next/head';
+
 export default function SignIn() {
   return (
-    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
-      <Flex
-        as="form"
-        w="100%"
-        maxWidth={360}
-        bg="gray.800"
-        p="8"
-        borderRadius={8}
-        flexDirection="column"
-      >
-        <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Senha" type="password" />
-        </Stack>
+    <>
+      <Head>
+        <title>SignIn | Dashgo</title>
+      </Head>
 
-        <Button type="submit" mt="6" colorScheme="orange" size="lg">
-          Entrar
-        </Button>
+      <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+        <Flex
+          as="form"
+          w="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDirection="column"
+        >
+          <Stack spacing="4">
+            <Input name="email" label="E-mail" type="email" />
+            <Input name="password" label="Senha" type="password" />
+          </Stack>
+
+          <Button type="submit" mt="6" colorScheme="orange" size="lg">
+            Entrar
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
